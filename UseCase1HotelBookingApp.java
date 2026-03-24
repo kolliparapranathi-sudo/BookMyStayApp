2380750d874a6afb8cbc6abcb16a126bac8c7081
Pranathi Kollipara <kolliparapranathi@gmail.com>
2026-03-24 09:21:37 +0530

    Added UC1 to UC6

---
+++ UseCase1HotelBookingApp.java
@@ -0,0 +1,10 @@
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Hotel Booking App ");
+        System.out.println(" Version : 1.0 ");
+        System.out.println("=================================");
+    }
+}
